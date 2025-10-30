66db09b8acf93de5dda4da226af186dd35bb4af1
GeantWorld <yanis.geantworld@gmail.com>
2025-10-30 22:12:16 +0100

    Create ModBlocks.java

---
+++ src/main/java/eu/geantworld/intothestars/block/ModBlocks.java
@@ -0,0 +1,11 @@
+package eu.geantworld.intothestars.block;
+
+import eu.geantworld.intothestars.StarsMod;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, StarsMod.MOD_ID);
+}
