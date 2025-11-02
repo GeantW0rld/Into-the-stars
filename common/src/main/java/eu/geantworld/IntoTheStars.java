f7a951fd03a724ef83a0c24b6065e0585ccb5508
GeantWorld <yanis.geantworld@gmail.com>
2025-11-02 18:31:08 +0100

    removed useless imports

--- common/src/main/java/eu/geantworld/IntoTheStars.java
+++ common/src/main/java/eu/geantworld/IntoTheStars.java
@@ -4,10 +4,9 @@ import eu.geantworld.into_the_stars.common.registry.ModBlocks;
 import eu.geantworld.into_the_stars.common.registry.ModItems;
 import eu.geantworld.into_the_stars.common.registry.ModMenus;
 import eu.geantworld.into_the_stars.common.registry.ModTabs;
-import net.minecraft.client.gui.screens.MenuScreens;
 
 public final class IntoTheStars {
-    public static final String MOD_ID = "into_the_stars";
+    // public static final String MOD_ID = "into_the_stars";
 
     public static void init() {
         ModBlocks.register();
